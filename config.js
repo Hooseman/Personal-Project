4e7c33c03ed01a486f7c1e8de0805a2bbe7f70cc
Juston Perkins <Hooseman@users.noreply.github.com>
2017-05-30 21:24:51 -0600

    swiched backend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  elephantsql:'postgres://yugpvokd:y6SH5X5Hr0EJLNgHhmjZw14RmB_oH24L@stampy.db.elephantsql.com:5432/yugpvokd',
+  elephantsql: "postgres://postgres:Perkinsfam12@localhost/postgres",
   port: 3000
 }
